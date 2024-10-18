44c8eadf9c2493f9026bf4380e6d3a08a9901f1b
Oğuz Atılgan <95192762+mckral68@users.noreply.github.com>
2024-10-18 20:59:25 +0300

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -2,11 +2,11 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+/*  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const loginPage = new URL("/admin/login", req.url).toString();
 
   // Eğer token yoksa kullanıcıyı giriş sayfasına yönlendir
- /* if (req.nextUrl.pathname === "/admin/login") {
+  if (req.nextUrl.pathname === "/admin/login") {
     return NextResponse.next();
   }
   if (!token) {
