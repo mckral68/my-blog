6c059d0f24195200aa538af9f31fec4a79af9a7b
Oğuz Atılgan <95192762+mckral68@users.noreply.github.com>
2025-01-12 02:26:15 +0300

    Update Admin Dashboardpage.jsx

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,8 +1,21 @@
+import Link from "next/link";
+
 const AdminPage = () => {
   return (
-    <div>
-      <h1>Admin Sayfası</h1>
-      <p>Hoş geldin!</p>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 shadow-lg rounded-lg text-center">
+        <h1 className="text-2xl font-bold text-gray-800 mb-6">Yönetim Paneli</h1>
+        {/* Linkler */}
+        <div className="space-y-4">
+          <Link href="/admin/posts" className="text-blue-500 hover:underline text-lg">
+            Gönderiler
+          </Link>
+          <br />
+          <Link href="/admin/messages" className="text-blue-500 hover:underline text-lg">
+            Mesajlar
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
